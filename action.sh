cf21157cd0370e43febf4ceed7aa51d4daa59fee
LCyberspazio <idmgiovanni@libero.it>
2014-03-02 22:49:54 +0100

    Added the core script

---
+++ action.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#DONT EXECUTE ME!
+#EXECUTE eyefinder.sh INSTEAD!!!
+
+#Script by Giovanni Santostefano
+#that found if a process is reading from the 1st webcam device
+#and if so, executes the script action.sh
+#Personalize action.sh to choose what action do you want to perform
+#in case of suspected webcam activation.
+
+echo "FOUND!"
+echo "Big brother is watching you!"
+echo "Last update on date"
+date
+
+echo "Big brother is watching you!" | wall
+
---
+++ eyefinder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Script by Giovanni Santostefano
+#that found if a process is reading from the 1st webcam device
+#and if so, executes the script action.sh
+#Personalize action.sh to choose what action do you want to perform
+#in case of suspected webcam activation.
+
+#if there are problems launch this script with root privileges
+
+while true
+do 
+clear 
+echo "Eyefinder by Giovanni Santostefano"
+echo "Searching for video stream..."
+echo
+
+
+ls -l -R /proc 2> /dev/null  | grep video0
+
+if [ $? -eq 0 ]
+then
+clear
+echo "Eyefinder by Giovanni Santostefano"
+echo
+sh action.sh
+fi
+
+sleep 10
+done
